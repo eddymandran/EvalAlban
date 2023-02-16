45e268328434d82d92cf518cad895788524cf5f2
eddymandran <eddy.mandran@gmail.com>
2023-02-16 11:37:41 +0100

    add seeder

--- database/seeders/SalesPeopleSeeder.php
+++ database/seeders/SalesPeopleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\SalesPeople;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,31 @@ class SalesPeopleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $datas = [
+            [
+                'lastname' => 'Pignon',
+                'firstname' => 'François',
+                'matricule' => '0123456789',
+                'email' => 'francois.pignon@dinner.con',
+            ],
+            [
+                'lastname' => 'Leblanc',
+                'firstname' => 'Juste',
+                'matricule' => '987654312',
+                'email' => 'juste.leblanc@dinner.con',
+            ],
+            [
+                'lastname' => 'Sassoeur',
+                'firstname' => 'Marlène ',
+                'matricule' => '346789654',
+                'email' => 'marlene.sassoeur@dinner.con',
+            ],
+
+
+        ];
+
+        foreach ($datas as $data) {
+            SalesPeople::create($data);
+        }
     }
 }
