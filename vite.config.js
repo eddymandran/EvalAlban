6f056afb8e661cbf0dd1c2440e8888b21dca5aa5
eddymandran <eddy.mandran@gmail.com>
2023-02-16 12:44:29 +0100

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        hmr: {
+            host: 'localhost'
+        },
+        watch: {
+            usePolling: true
+        }
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
